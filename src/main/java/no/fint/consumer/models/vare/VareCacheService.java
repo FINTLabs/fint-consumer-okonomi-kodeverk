e1819a9f0f94bcf91715d82699b871114268c91a
Sander Nilsen Rysjedal <117441169+Sander14121@users.noreply.github.com>
2024-04-15 13:56:16 +0200

    update dependencies & new relations (#17)

--- src/main/java/no/fint/consumer/models/vare/VareCacheService.java
+++ src/main/java/no/fint/consumer/models/vare/VareCacheService.java
@@ -107,7 +107,10 @@ public class VareCacheService extends CacheService<VareResource> {
         } else {
             data = objectMapper.convertValue(event.getData(), javaType);
         }
-        data.forEach(linker::mapLinks);
+        data.forEach(resource -> {
+            linker.mapLinks(resource);
+            linker.resetSelfLinks(resource);
+        });
         if (KodeverkActions.valueOf(event.getAction()) == KodeverkActions.UPDATE_VARE) {
             if (event.getResponseStatus() == ResponseStatus.ACCEPTED || event.getResponseStatus() == ResponseStatus.CONFLICT) {
                 List<CacheObject<VareResource>> cacheObjects = data
