6747de7e7c31099a42a6c78b4cf2d31c598a76c4
Trond Magnus Sevre <trond@fintlabs.no>
2024-06-18 09:47:52 +0200

    Version 3.18.0.

--- src/main/java/no/fint/consumer/models/merverdiavgift/MerverdiavgiftCacheService.java
+++ src/main/java/no/fint/consumer/models/merverdiavgift/MerverdiavgiftCacheService.java
@@ -107,10 +107,7 @@ public class MerverdiavgiftCacheService extends CacheService<MerverdiavgiftResou
         } else {
             data = objectMapper.convertValue(event.getData(), javaType);
         }
-        data.forEach(resource -> {
-            linker.mapLinks(resource);
-            linker.resetSelfLinks(resource);
-        });
+        data.forEach(linker::mapLinks);
         if (KodeverkActions.valueOf(event.getAction()) == KodeverkActions.UPDATE_MERVERDIAVGIFT) {
             if (event.getResponseStatus() == ResponseStatus.ACCEPTED || event.getResponseStatus() == ResponseStatus.CONFLICT) {
                 List<CacheObject<MerverdiavgiftResource>> cacheObjects = data
--- src/main/java/no/fint/consumer/models/vare/VareController.java
+++ src/main/java/no/fint/consumer/models/vare/VareController.java
@@ -182,7 +182,7 @@ public class VareController {
 
             fintAuditService.audit(response, Status.SENT_TO_CLIENT);
 
-            return linker.toResource(vare);
+            return linker.mapAndResetLinks(vare);
         }    
     }
 
