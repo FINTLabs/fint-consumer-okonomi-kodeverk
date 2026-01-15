6938fdce78b4cf6c76df8f68c9e84d53e320c715
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2026-01-15 15:29:19 +0100

    Version 4.0.0-rc-6.

---
+++ src/main/java/no/novari/fint/consumer/config/Constants.java
@@ -0,0 +1,18 @@
+package no.novari.fint.consumer.config;
+
+public enum Constants {
+;
+
+    public static final String COMPONENT = "okonomi-kodeverk";
+    public static final String COMPONENT_CONSUMER = COMPONENT + " consumer";
+    public static final String CACHE_SERVICE = "CACHE_SERVICE";
+
+    
+    public static final String CACHE_INITIALDELAY_MERVERDIAVGIFT = "${fint.consumer.cache.initialDelay.merverdiavgift:900000}";
+    public static final String CACHE_FIXEDRATE_MERVERDIAVGIFT = "${fint.consumer.cache.fixedRate.merverdiavgift:900000}";
+    
+    public static final String CACHE_INITIALDELAY_VARE = "${fint.consumer.cache.initialDelay.vare:1000000}";
+    public static final String CACHE_FIXEDRATE_VARE = "${fint.consumer.cache.fixedRate.vare:900000}";
+    
+
+}
